b2d3e7e1dc88c95926d77a24b8361c1e9241f106
Cameron Chandler <webstormcam@gmail.com>
2022-12-26 12:22:37 -0500

    Starting the JS section

---
+++ script.js
@@ -0,0 +1,23 @@
+let home = document.getElementById('home-score')
+let guest = document.getElementById('guest-score')
+
+
+
+
+
+
+
+
+
+
+function add1(){
+
+}
+
+function add2(){
+    
+}
+
+function add3(){
+    
+}
