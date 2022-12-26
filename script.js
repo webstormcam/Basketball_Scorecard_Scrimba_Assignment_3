0602769bd8e7b7f6b7d59e8aa3518fcf04c3e618
Cameron Chandler <webstormcam@gmail.com>
2022-12-26 13:31:32 -0500

    Trying to make winning check work

--- script.js
+++ script.js
@@ -7,35 +7,48 @@ let guestCount=0;
 function addOneHome(){
     homeCount+=1
     home.innerHTML=homeCount
+    winningCheck()
 }
 
 function addTwoHome(){
     homeCount+=2
     home.innerHTML=homeCount
+    winningCheck()
 }
 
 function addThreeHome(){
     homeCount+=3
     home.innerHTML=homeCount
+    winningCheck()
 }
 
 function addOneGuest(){
     guestCount+=1
     guest.innerHTML=guestCount
+    winningCheck()
 }
 
 function addTwoGuest(){
     guestCount+=2
     guest.innerHTML=guestCount
+    winningCheck()
 }
 function addThreeGuest(){
     guestCount+=3
     guest.innerHTML=guestCount
+    winningCheck()
 }
 
 
 function winningCheck(){
-    
+    if(homeCount>guestCount){
+        home.style.border="1px solid yellow"
+    } else if(guestCount>homeCount){
+        guest.style.border="1px solid yellow"
+    } else if(homeCount===guestCount){
+        home.style.border="none"
+        guest.style.border="none"
+    }
 }
 
 function reset(){
