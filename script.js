b8eb89f503607ce65f320ec284630c2fa1146fa3
Cameron Chandler <webstormcam@gmail.com>
2022-12-26 12:59:40 -0500

    Added in some JS

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 let home = document.getElementById('home-score')
+let homeCount=0;
 let guest = document.getElementById('guest-score')
+let guestCount=0;
 
 
-
-
-
-
-
-
-
-
-function add1(){
-
+function addOneHome(){
+    homeCount+=1
+    home.innerHTML=homeCount
 }
 
-function add2(){
+function addTwoHome(){
+    homeCount+=2
+    home.innerHTML=homeCount
+}
+
+function addThreeHome(){
+    homeCount+=3
+    home.innerHTML=homeCount
+}
+
+function addOneGuest(){
+    guestCount+=1
+    guest.innerHTML=guestCount
+}
+
+function addTwoGuest(){
+    guestCount+=2
+    guest.innerHTML=guestCount
+}
+function addThreeGuest(){
+    guestCount+=3
+    guest.innerHTML=guestCount
+}
+
+
+function winningCheck(){
     
 }
 
-function add3(){
-    
+function reset(){
+    homeCount=0;
+    guestCount=0;
+    home.innerHTML=homeCount
+    guest.innerHTML=guestCount
 }
+
+
+
+
+
+
+
+
+
