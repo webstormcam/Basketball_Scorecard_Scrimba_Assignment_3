61e1ebb32f23aa18145ab8e8003985edae16b6b3
Cameron Chandler <webstormcam@gmail.com>
2022-12-28 23:50:48 -0500

    Updated functions
    Made it more dynamic

--- script.js
+++ script.js
@@ -4,40 +4,16 @@ let guest = document.getElementById('guest-score')
 let guestCount=0;
 
 
-function addOneHome(){
-    homeCount+=1
-    home.innerHTML=homeCount
-    winningCheck()
-}
-
-function addTwoHome(){
-    homeCount+=2
-    home.innerHTML=homeCount
-    winningCheck()
-}
-
-function addThreeHome(){
-    homeCount+=3
-    home.innerHTML=homeCount
-    winningCheck()
-}
-
-function addOneGuest(){
-    guestCount+=1
-    guest.innerHTML=guestCount
-    winningCheck()
-}
-
-function addTwoGuest(){
-    guestCount+=2
-    guest.innerHTML=guestCount
-    winningCheck()
-}
-function addThreeGuest(){
-    guestCount+=3
-    guest.innerHTML=guestCount
-    winningCheck()
-}
+document.addEventListener('click', (e)=> {
+    if (e.target.id === "home") { 
+        homeCount+= Number(e.target.value)
+        home.innerHTML = homeCount
+    } else if (e.target.id === "guess") {
+        guestCount+=Number(e.target.value)
+        guest.innerHTML=guestCount
+    }
+     winningCheck()
+})
 
 
 function winningCheck(){
